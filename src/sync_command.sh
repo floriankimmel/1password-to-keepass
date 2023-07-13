c28386da882547a52ec5833eec4259326cc0e84d
Florian Kimmel <florianmkimmel@gmail.com>
2023-07-13 08:54:47 +0200

    Use secret reference to read secret

--- src/sync_command.sh
+++ src/sync_command.sh
@@ -1,7 +1,7 @@
 vault=${args[--vault]}
 keepassDatabase=${args[--keepass-database]}
 keepassSecret=${args[--keepass-secret]}
-keepassPassword=$(op item get --vault $vault $keepassSecret --fields label=password)
+keepassPassword=$(op read $keepassSecret)
 
 op item --vault $vault list --format json | jq -r '.[].title' | while IFS= read -r title; do
     if printf '%s\n' "$keepass" | keepassxc-cli search  $keepassDatabase "$title" 2>&1 | grep -q "No results for that search term"; then
